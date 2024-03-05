ad61bece8fb3e775212ce0f923cf262ce5a848d6
joaopedroserrano <joaops.pt@gmail.com>
2024-03-05 18:19:17 +0000

    Delete playful-violet-bananas/playful-violet-bananas directory

--- playful-violet-bananas/playful-violet-bananas/Pages/private/MyRecipes.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function MyRecipes() {
-  return (
-    <View  style={{justifyContent: "center",alignItems:"center",height: "100%"}}>
-      <Text> My Recipes </Text>
-    </View>
-  );
-}
--- playful-violet-bananas/playful-violet-bananas/Pages/private/SearchRecipe.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function SearchRecipes() {
-  return (
-    <View  style={{justifyContent: "center",alignItems:"center",height: "100%"}}>
-      <Text> Search Recipes </Text>
-    </View>
-  );
-}
