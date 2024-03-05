b956e8be715811154970522afaec4febc93f7e41
joaopedroserrano <joaops.pt@gmail.com>
2024-03-05 18:18:15 +0000

    Add files via upload

---
+++ playful-violet-bananas/playful-violet-bananas/Pages/private/MyRecipes.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function MyRecipes() {
+  return (
+    <View  style={{justifyContent: "center",alignItems:"center",height: "100%"}}>
+      <Text> My Recipes </Text>
+    </View>
+  );
+}
