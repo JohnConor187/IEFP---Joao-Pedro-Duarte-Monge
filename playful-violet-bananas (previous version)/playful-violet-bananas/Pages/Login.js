f1941176fe44a99f9c75405cdb1c6bc571b8af98
JohnConor187 <slicer.xyz@gmail.com>
2024-03-05 21:54:58 +0000

    Add files via upload

---
+++ playful-violet-bananas (previous version)/playful-violet-bananas/Pages/Login.js
@@ -0,0 +1,72 @@
+import {
+  ImageBackground,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  TextInput,
+} from 'react-native';
+
+export default function Login({navigation}) {
+  return (
+    <ImageBackground
+      source={{
+        uri: 'https://virtualbackgrounds.site/wp-content/uploads/2020/07/cooking-in-a-white-kitchen.jpg',
+      }}
+      style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+      resizeMode="cover">
+      <View
+        style={{
+          flex: 1,
+          width: '100%',
+          backgroundColor: 'rgba(0,0,0,0.6)',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <TextInput
+          style={styles.txtInput}
+          placeholder="Username"
+          inputMode="email"></TextInput>
+        <TextInput
+          style={styles.txtInput}
+          placeholder="Password"
+          secureTextEntry={true}></TextInput>
+
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btnTxt}>Login</Text>
+        </TouchableOpacity>
+          <Text
+          style={[styles.btnTxt, { fontSize: 16, marginTop: 10 }]}
+          onPress={() => navigation.navigate("PassRecover") }>
+          Forgot your password? Click here
+        </Text>
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  btn: {
+    width: '50%',
+    backgroundColor: '#fc8c03',
+    color: 'white',
+    height: 50,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+  },
+  btnTxt: {
+    color: 'white',
+    fontSize: 20,
+  },
+  txtInput: {
+    width: '80%',
+    backgroundColor: 'rgba(255,255,255,0.5)',
+    color: 'white',
+    height: 40,
+    borderRadius: 15,
+    paddingLeft: 10,
+    margin: 10,
+  },
+});
